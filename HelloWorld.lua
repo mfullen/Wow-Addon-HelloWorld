29d3feb4d38bc691e76e347f585d5192d1787fac
Mike Fullen <mike@mikefullen.com>
2014-10-22 11:06:40 -0400

    First Commit

---
+++ HelloWorld.lua
@@ -0,0 +1,16 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: ifull
+-- Date: 10/22/14
+-- Time: 10:36 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+function HelloWorld()
+
+    --local b = IsAddOnLoaded("foo");
+    local x = NUM_CHAT_WINDOWS;
+    print "Hello World, Num Chat Windows"
+    print (x);
+end
+
